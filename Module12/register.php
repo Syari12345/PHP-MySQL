3ae0e1519ac5e2306e24dae35020ec2713c3d176
Syari12345 <syari.aliu@gmail.com>
2026-09-23 23:26:54 +0200

    Module12

--- Module12/register.php
+++ Module12/register.php
@@ -10,23 +10,33 @@ if(isset($_POST['submit'])){
 
     $hashed_password=password_hash($password,_PASSWORD_BCRYPT);
 
-    if(empty($name)
-    empty($surname)
-    empty($username)
-    empty($email)
+    if(empty($name)||
+    empty($surname)||
+    empty($username)||
+    empty($email)||
     empty($password)){
         echo "You need to fill all data";
-  
     }else{
-        $sql="SELECT * FROM user_login where email='$email" CR username='$username'";
+        $sql="SELECT * FROM user_login where email='$email" OR username='$username'";
 
         $tempSQL=$conn->prepare($sql);
         $tempSQL->execute();
 
         if($tempSQL->rowCount()>0){
-        
+        echo "This username or email already exists!";
+        header("refresh:2; url=signup.php");  
     }
+     else{
+        $sql="INSERT INTO user_login(name,surname,username,email,password) VALUES ('$name','$surname','username','$email','$hashed_password')";
+
+        $insertSql=$conn->prepare($sql);
+        $insertSql->execute();
+
+        echo "New user is created successfully!"
+        header("refresh:2; url=login.php");
     
   
-}
+      }
+        }
+    }
 ?>
